1e2f8509b9380d114c1ee8619cfb62b20b44404e
Guillermo <guillermogarnica94@gmail.com>
2024-09-12 19:39:44 +0200

    Modifico index y script

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const openUrlButton = document.getElementById('openUrlButton');
-    const urlInput = document.getElementById('urlInput');
 
     const botonBuscarPelicula = document.getElementById('botonObtenerDatos');
+    const pelicula = document.getElementById('pelicula');
 
-    openUrlButton.addEventListener('click', () => {
-        const url = urlInput.value.trim();
-        if (url) {
-            // Verifica que la URL comience con http:// o https://
-            const validUrl = /^https?:\/\//i.test(url) ? url : 'http://' + url;
-            window.open(validUrl, '_blank');
-        } else {
-            alert('Por favor, escribe una URL válida.');
-        }
-    });
+    botonBuscarPelicula.addEventListener('click', buscaDatos);
+
+    function buscaDatos(){
+      const url = 'https://omdbapi.com/?t=' + pelicula;
+      window.open(url, '_blank');
+    }
+
 });
