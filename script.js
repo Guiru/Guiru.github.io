3c96347bab38161cd3150b0e4bb3a7407e107053
Guillermo <guillermogarnica94@gmail.com>
2024-09-12 19:21:25 +0200

    Añado script y modifico pagina

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openUrlButton = document.getElementById('openUrlButton');
+    const urlInput = document.getElementById('urlInput');
+
+    openUrlButton.addEventListener('click', () => {
+        const url = urlInput.value.trim();
+        if (url) {
+            // Verifica que la URL comience con http:// o https://
+            const validUrl = /^https?:\/\//i.test(url) ? url : 'http://' + url;
+            window.open(validUrl, '_blank');
+        } else {
+            alert('Por favor, escribe una URL válida.');
+        }
+    });
+});
