665054568c0244c3e16835c6c9e0f45874db3dcb
Guillermo <guillermogarnica94@gmail.com>
2024-09-12 20:04:18 +0200

    Modifico script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
     const botonBuscarPelicula = document.getElementById('botonObtenerDatos');
     const pelicula = document.getElementById('pelicula');
-
+   
     botonBuscarPelicula.addEventListener('click', buscaDatos);
 
     function buscaDatos(){
-      const url = concat("https://omdbapi.com/?t=",pelicula,"?apikey=b62fe9a9");
+      const url = 'https://omdbapi.com/?t=' + pelicula + '?apikey=b62fe9a9');
       window.open(url, '_blank');
     }
